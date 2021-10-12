5ce16f38bb7b4c462d42acef8b95cbd61aff626d
Your Name <you@example.com>
2021-10-12 14:48:50 +0700

    add

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        try {
+            while (numberGenerator.getThread().isAlive()) {
+                System.out.println("Hello");
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("interrupted");
+        }
+        System.out.println("Over");
+    }
+}
